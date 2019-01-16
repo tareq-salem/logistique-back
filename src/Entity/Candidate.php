efc3046a1cc027cddf8bcdfc1c028dca0e5702f5
Julien Laforge <julien.laforge@gmx.fr>
2019-01-16 16:33:03 +0100

    path superclass fix

--- src/Entity/Candidate.php
+++ src/Entity/Candidate.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Entity\SuperClass as SuperClass;
+use App\Entity\SuperClass as SuperClass;
 
 
 /**
