4319c7e63beeabb1ef1734d221d1201bb5eb8b8a
Julien Laforge <julien.laforge@gmx.fr>
2019-01-17 12:25:44 +0100

    fix migrations build conflicts

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\SuperClassRepository as SuperClass;
+use App\Entity\SuperClass as SuperClass;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
