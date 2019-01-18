546fdf3e3361196eaa5acbfd035a0efc89a837ad
tareq-salem <38789773+tareq-salem@users.noreply.github.com>
2019-01-18 17:08:59 +0100

    Fixtures + Superclass : MappedSuperclass typo

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -27,156 +27,78 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-//        $faker = Factory::create('fr_FR');
         //CREATION DE L'ADMIN
-        $dateTime = new \DateTime();
+        $dateTime = new \DateTime('now', new DateTimeZone('Europe/Paris'));
         $admin = new User();
         $admin->setLogin('admin');
-        //$password = $this->encoder->encodePassword($admin, 'password');
-        $admin->setPassword('password');
-        $admin->setCreatedAt($dateTime);
-        $admin->setModifiedAt($dateTime);
-
+        $password = $this->encoder->encodePassword($admin, 'password');
+            
         $manager->persist($admin);
         $manager->flush();
 
+        $faker = Factory::create('fr_FR');
 
-        //CREATION DE STATUS
-        $statusCadre = new Status();
-        $statusCadre->setName('Cadre');
-        $statusCadre->setIsActive(true);
-
-        $statusOuvrier = new Status();
-        $statusOuvrier->setName('Ouvrier');
-        $statusOuvrier->setIsActive(true);
-
-        $statusAlternant= new Status();
-        $statusAlternant->setName('Alternant');
-        $statusAlternant->setIsActive(true);
-
-        $manager->persist($statusCadre);
-        $manager->persist($statusOuvrier);
-        $manager->persist($statusAlternant);
-
+        //Faker table STATUS
+        for ($i = 0; $i < 10; $i++)
+        {
+            $status = new Status();
+            $status->setName($faker->randomElements($statusName, $count = 1));
+            $status->setIsActive($faker->boolean);
+            $manager->persist($status);
+        }
         $manager->flush();
 
-        //Creation Contrat
-
-        $contractCdi = new ContractType();
-        $contractCdi->setName('CDI');
-        $contractCdi->setColor('red');
-
-        $contractCdd = new ContractType();
-        $contractCdd->setName('CDD');
-        $contractCdd->setColor('blue');
-
-        $contractInterimaire= new ContractType();
-        $contractInterimaire->setName('Interimaire');
-        $contractInterimaire->setColor('pink');
-
-        $manager->persist($contractCdi);
-        $manager->persist($contractCdd);
-        $manager->persist($contractInterimaire);
-
+        //Faker table Contrat
+        for ($i = 0; $i < 10; $i++)
+        {
+            $contractType = new ContractType();
+            $status->setName($faker->randomElements($contractTypeName, $count = 1));
+            $status->setColor($faker->hexcolor);
+            $manager->persist($contractType);
+        }
         $manager->flush();
 
-        //Creation Contrat
+        //Faker table OfferType
+        $offerTypeName = array('Ouvrier', 'Commercial', 'Ingenieur');
 
-        $offerTypeOuvrier = new OfferType();
-        $offerTypeOuvrier->setName('Ouvrier');
-        $offerTypeOuvrier->setIsActive(true);
-
-        $offerTypeCommercial = new OfferType();
-        $offerTypeCommercial->setName('Commercial');
-        $offerTypeCommercial->setIsActive(true);
-
-        $offerTypeIngenieur= new OfferType();
-        $offerTypeIngenieur->setName('Ingenieur');
-        $offerTypeIngenieur->setIsActive(true);
-
-        $manager->persist($offerTypeOuvrier);
-        $manager->persist($offerTypeCommercial);
-        $manager->persist($offerTypeIngenieur);
-
+        for ($i = 0; $i < 10; $i++)
+        {
+            $offerType = new OfferType();
+            $offerType->setName($faker->randomElements($offerTypeName, $count = 1));
+            $offerType->setIsActive($faker->boolean);
+            $manager->persist($offerType);
+        }
         $manager->flush();
 
+        // Faker table Offer
+        $contractTypeName = array ('CDI','CDD','INTERIM', 'Contrat de professionnalisation','Contrat d\'apprentissage');
 
+        $titleOffer = array ('Ouvrier specialise','Préparateur(trice) de commande (H/F) CDI/CDD',
+                            'Ingénieur(e) qualité (H/F) CDI/CDD','Assistant(e) service client (H/F) CDI/CDD',
+                            'Alternant Assistant Logistique (H/F) ', 'Directeur Logistique (H/F) CDD',
+                            'Responsable exploitation plateforme logistique (H/F) CDI');
 
-        //CREATION D'OFFERS
-        $salary = ['1100 a 1500', '1500 a 2000', '2000 a 3000'];
-        $title = ['je', 'sais', 'rien'];
-        $hourPerWeek = ['15h','20h', '35h'];
-        $duration = ['six month', 'one year','three year'];
-        $availability = ['yes', 'no'];
+        $statusName = array ('Cadre','Ouvrier','Alternant');
 
-        $benefits = array("Nulla facilisi. Cras non velit nec nisi",
-            "Phasellus sit amet erat. Nulla tempus",
-            "rtor risus dapibus augue, vel accumsan tellus nisi eu orci.",
-            "interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
-            "pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis.",
-            "apien quis libero.",
-            "us purus, aliquet at, feugiat non, pretium quis, lectus.",
-            "consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum.",
-            "sapien cursus vestibulum. Proin eu mi. Nulla ac enim.",
-            ", nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.",
-            "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.",
-            "lit ac nulla. Sed vel enim sit amet nunc viverra dapibus.",
-            "Morbi non quam nec dui luctus rutrum.");
-
-        $description = array("Nulla facilisi. Cras non velit nec nisi",
-            "Phasellus sit amet erat. Nulla tempus",
-            "rtor risus dapibus augue, vel accumsan tellus nisi eu orci.",
-            "interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
-            "pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis.",
-            "apien quis libero.",
-            "us purus, aliquet at, feugiat non, pretium quis, lectus.",
-            "consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum.",
-            "sapien cursus vestibulum. Proin eu mi. Nulla ac enim.",
-            ", nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.",
-            "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.",
-            "lit ac nulla. Sed vel enim sit amet nunc viverra dapibus.",
-            "Morbi non quam nec dui luctus rutrum.");
-
-        $requiredProfil = array("Nulla facilisi. Cras non velit nec nisi",
-            "PhaselNulla tempus",
-            "rtor risus dapibus augue, vel accumsan tellus nisi eu orci.",
-            "interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
-            "pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis.",
-            "apien quis libero.",
-            "us purus, aliquet at, feugiat non, pretium quis, lectus.",
-            "consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum.",
-            "sapien cursus vestibulum. Proin eu mi. Nulla ac enim.",
-            ", nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.",
-            "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat.",
-            "lit ac nulla. Sed vel enim sit amet nunc viverra dapibus.",
-            "Morbi non quam nec dui luctus rutrum.");
-
-
-
-        $contractType = $manager->getRepository(ContractType::class)->findAll();
-        $offerType = $manager->getRepository(OfferType::class)->findAll();
-        $status = $manager->getRepository(Status::class)->findAll();
-
-
-        for ($i = 0; $i < 20; $i++) {
-            $dateTime = new \DateTime();
+        for ($i = 0; $i < 10; $i++) {
+            $dateTime = new \DateTime('now', 'Europe/Paris');
             $offer = new Offer();
-            $offer->setAvailability($availability[mt_rand(0, 1)]);
-            $offer->setBenefits($benefits[mt_rand(0, 10)]);
-            $offer->setContratType($contractType[mt_rand(0, 2)]);
-            $offer->setDescription($description[mt_rand(0, 10)]);
-            $offer->setDuration( $duration[mt_rand(0, 2)]);
-            $offer->setEndPublicationDate($dateTime);
-            $offer->setHourPerWeek($hourPerWeek[mt_rand(0, 2)]);
-            $offer->setIsActive(true);
-            $offer->setOfferType($offerType[mt_rand(0, 2)]);
-            $offer->setReference("referenceAdmin");
-            $offer->setRequiredExperience("experience");
-            $offer->setRequiredProfil($requiredProfil[mt_rand(0, 10)]);
-            $offer->setSalary($salary[mt_rand(0, 2)]);
-            $offer->setTitle($title[mt_rand(0, 2)]);
+            $offer->setAvailability($faker->numerify('# Mois'));
+            $offer->setBenefits($faker->sentences(3, false));
+            $offer->setContratType($faker->randomElements($contractTypeName, $count = 1));
+            $offer->setDescription($faker->text(200));
+            $offer->setDuration($faker->numerify('## Mois'));
+            $offer->setEndPublicationDate($faker->DateTime('2019-02-28 13:46:23', 'Europe/Paris'));
+            $offer->setHourPerWeek($faker->numerify('## par semaine'));
+            $offer->setIsActive($faker->boolean);
+            $offer->setOfferType($faker->jobTitle);
+            $offer->setReference($faker->swiftBicNumber);
+            $offer->setRequiredExperience($faker->numerify("# ans"));
+            $offer->setRequiredProfil($faker->text($maxNbChars = 15));
+            $offer->setSalary($faker->numerify("#### €/mois"));
+            $offer->setTitle($faker->randomElements($titleOffer, $count = 1));
             $offer->setStartPublicationDate($dateTime);
-            $offer->setStatus($status[mt_rand(0, 2)]);
+            $offer->setStatus($faker->randomElements($statusName, $count = 1));
             $manager->persist($offer);
         }
         $manager->flush();
--- src/Entity/SuperClass.php
+++ src/Entity/SuperClass.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\MappedSuperClass
+ * @ORM\MappedSuperclass
  */
 class SuperClass
 {
