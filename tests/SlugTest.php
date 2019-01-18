7012d3aee2db424a6cdf77457a54631fb2b308e4
Julien Laforge <julien.laforge@gmx.fr>
2019-01-18 16:38:49 +0100

    create slugger service

---
+++ tests/SlugTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Utils\Slugger;
+
+class SlugTest extends TestCase
+{
+    public function test1()
+    {
+        $strToTest = 'une chaine de caractere a slugger';
+        $slugger = new Slugger();
+        $strExpected = 'une-chaine-de-caractere-a-slugger';
+        $this->assertSame($strExpected, $slugger->slugify($strToTest));
+    }
+
+    public function test2()
+    {
+        $strToTest = 'UNE CHAINE A SLUGGER';
+        $slugger = new Slugger();
+        $strExpected = 'une-chaine-a-slugger';
+        $this->assertSame($strExpected, $slugger->slugify($strToTest));
+    }
+
+    public function test3()
+    {
+        $strToTest = 'Une chaine de caractère à slugger';
+        $slugger = new Slugger();
+        $strExpected = 'une-chaine-de-caractere-a-slugger';
+        $this->assertSame($strExpected, $slugger->slugify($strToTest));
+    }
+
+    public function test4()
+    {
+        $strToTest = 'Une chaine de caractère à re-slugger';
+        $slugger = new Slugger();
+        $strExpected = 'une-chaine-de-caractere-a-re-slugger';
+        $this->assertSame($strExpected, $slugger->slugify($strToTest));
+    }
+
+
+    public function test5()
+    {
+        $strToTest = '\\a_àéàç\'_(è';
+        $slugger = new Slugger();
+        $strExpected = '-a-aeac-e';
+        $this->assertSame($strExpected, $slugger->slugify($strToTest));
+    }
+}
