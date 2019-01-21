4fb36a3d4057536871c9a5e4ba877f88687fe6e7
Vero-Morane Lafaverges <v.lafaverges@gmail.com>
2019-01-21 17:36:34 +0100

    dashboard && admin_role ok --  listing des repository order by descendant des 5 dernierers valeurs

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,19 +2,47 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Offer;
+use App\Repository\CandidateRepository;
+use App\Repository\CandidatureRepository;
+use App\Repository\OfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     /**
+     * DASHBOARD
      * @Route("/admin", name="admin")
      */
-    public function index()
+    public function index(OfferRepository $offerRepository, CandidatureRepository $candidatureRepository)
     {
+
+       // * @method Offer[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+/*
+        $offers = $offerRepository->findBy([
+            ["created_at"],
+            ["ASC"],
+            5,
+            null
+            ]);
+
+        $candidatures = $candidatureRepository->findBy([
+            ["created_at"],
+            ["ASC"],
+            5,
+            null
+            ]);
+*/
+
+        $offers = $offerRepository->findByLatestLimitedBy(5);
+        $candidatures = $candidatureRepository->findByLatestLimitedBy(5);
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
+            'offers' => $offers,
+            'candidatures' => $candidatures
         ]);
     }
+
 
 }
