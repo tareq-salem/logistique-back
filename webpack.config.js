39b258879d1bf18a38a9346b5d84cc034545fa65
Faresse TAHLAITI <hafidhou.mdj@gmail.com>
2019-01-23 15:33:41 +0100

    dashboard

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ Encore
     .addStyleEntry('contact', './assets/scss/contact.scss')
     .addStyleEntry('candidature', './assets/scss/candidature.scss')
     .addStyleEntry('login', './assets/scss/login.scss')
+    .addStyleEntry('dashboard', './assets/scss/dashboard.scss')
 
     //.addEntry('app', './assets/js/app.js')
     //.addEntry('page1', './assets/js/page1.js')
