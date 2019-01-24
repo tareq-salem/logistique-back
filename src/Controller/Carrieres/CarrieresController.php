ba4ed3a0e746105dff15fdfc81723efc26f6a40d
Quentin Dudon <qdudon@gmail.com>
2019-01-24 15:12:03 +0100

    Postuler (#65)
    * postuler finito
    
    * postuler en cours

--- src/Controller/Carrieres/CarrieresController.php
+++ src/Controller/Carrieres/CarrieresController.php
@@ -9,6 +9,7 @@
     use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
+    // use Symfony\Component\HttpFoundation\Request;
 
     class CarrieresController extends AbstractController
     {
--- src/Form/PostulerType.php
+++ src/Form/PostulerType.php
@@ -5,22 +5,23 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class PostulerType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('created_at')
-            ->add('modified_at')
-            ->add('submit_date')
-            ->add('message')
-            ->add('cover_letter')
-            ->add('resume')
-            ->add('is_active')
-            ->add('candidate')
-            ->add('locationOffer')
-        ;
+            ->add('email')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('message', TextareaType::class, [
+                'attr' => ['class' => 'tinymce'],
+            ])
+            ->add('cv', FileType::class)
+            ->add('lm', FileType::class)
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
